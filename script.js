640a689d328593b4696bfdf68011e57e576d4f29
Guilherme Araújo <132411540+Slotov-7@users.noreply.github.com>
2024-04-29 10:17:30 -0300

    botando os textop

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Estudante de Ciência da computação'],
+    strings: ['Desemvolvedor Back-End Trainee', 'Estudante de Ciência da Computação' ],
     typedSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
